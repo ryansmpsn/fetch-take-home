859bfd7f46ec9ece9e56b6dda618fced46154ca0
Ryan Simpson <ryzizn@gmail.com>
2025-02-04 23:48:59 -0500

    feat: click outside hook

---
+++ src/hooks/useOutsideClick.ts
@@ -0,0 +1,23 @@
+import type { RefObject } from 'react';
+import { useEffect } from 'react';
+
+function useOutsideClick<T extends HTMLElement>(
+  ref: RefObject<T>,
+  callback: () => void
+) {
+  const handleClick = (e: Event) => {
+    if (ref.current && !ref.current.contains(e.target as Node)) {
+      callback();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleClick);
+
+    return () => {
+      document.removeEventListener('click', handleClick);
+    };
+  });
+}
+
+export default useOutsideClick;
