d2c7311e70b8b43cf2e9d163a6c69deb91df3c58
Ryan Simpson <ryzizn@gmail.com>
2025-02-05 00:49:22 -0500

    feat: main search page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,87 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+
+import SearchResultsList from '@/components/SearchResultsList';
+import { fetcher } from '@/api/ApiClient';
+import { Dog } from '@/types';
+import { useQuery } from '@tanstack/react-query';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import Select, { MultiValue } from 'react-select';
+import Link from 'next/link';
+
+const fetchBreeds = async (): Promise<string[] | undefined> => {
+  const data = await fetcher('/dogs/breeds', {
+    method: 'GET'
+  });
+
+  return data as string[];
+};
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const router = useRouter();
+  const [selectedBreeds, setSelectedBreeds] = useState<Dog['breed'][]>([]); // State to store selected breeds
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+  const {
+    data: breeds,
+    error,
+    isLoading,
+    isError
+  } = useQuery({
+    queryFn: () =>
+      fetchBreeds().catch((error) => {
+        if (error instanceof Error && error.message === 'Unauthorized') {
+          router?.push('/login'); // Redirect to login if unauthorized
+        }
+
+        throw new Error('Failed to fetch breeds. Please try again later.');
+      }),
+    queryKey: ['breeds']
+  });
+
+  const handleBreedChange = (
+    newValue: MultiValue<{ value: string; label: string }>
+  ) => {
+    const selectedValues = newValue
+      ? newValue.map((option) => option.value)
+      : [];
+    setSelectedBreeds(selectedValues);
+  };
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <div>
+      <h1>Select Breeds</h1>
+      <div>
+        <Select
+          options={breeds?.map((breed) => ({ value: breed, label: breed }))}
+          isMulti
+          onChange={handleBreedChange}
+          value={selectedBreeds.map((breed) => ({
+            value: breed,
+            label: breed
+          }))}
+          placeholder="Select breeds..."
+        />
+      </div>
+      <h1>Search Results</h1>
+      <div>
+        <h2>Select Favorite Dogs</h2>
+        <div>
+          <SearchResultsList />
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+      <div>
+        <h2>Favorited Dogs: TODO Make this a separate page</h2>
+
+        <Link href="/favorites">review favorited dogs</Link>
+      </div>
     </div>
   );
 }
