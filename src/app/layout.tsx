8a7575183d4c4bcf87c9c04c334e06372f31e526
Ryan Simpson <ryzizn@gmail.com>
2025-02-05 22:36:28 -0500

    feat: roboto font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Roboto } from 'next/font/google';
 import Providers from './providers';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin']
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin']
+const roboto = Roboto({
+  variable: '--font-roboto',
+  subsets: ['latin'],
+  weight: ['100', '300', '400', '500', '700']
 });
 
 export const metadata: Metadata = {
@@ -23,8 +19,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="en" style={{ all: 'unset' }}>
+      <body
+        className={`${roboto.variable}`}
+        style={{ all: 'unset', fontFamily: 'var(--font-roboto)' }}
+      >
         <Providers>{children}</Providers>
       </body>
     </html>
