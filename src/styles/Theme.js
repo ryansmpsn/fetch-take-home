7d502450bcbebbba5a3fd7be4e510847d1454b11
Ryan Simpson <ryzizn@gmail.com>
2025-02-05 20:13:49 -0500

    fix: export default theme

---
+++ src/styles/Theme.js
@@ -0,0 +1,28 @@
+const size = {
+  mobileS: '320px',
+  mobileM: '375px',
+  mobileL: '425px',
+  tablet: '768px',
+  laptop: '1024px',
+  laptopL: '1440px',
+  desktop: '2560px'
+};
+
+const device = {
+  mobileS: `(max-width: ${size.mobileS})`,
+  mobileM: `(max-width: ${size.mobileM})`,
+  mobileL: `(max-width: ${size.mobileL})`,
+  tablet: `(max-width: ${size.tablet})`,
+  laptop: `(max-width: ${size.laptop})`,
+  laptopL: `(max-width: ${size.laptopL})`,
+  desktop: `(max-width: ${size.desktop})`
+};
+
+export const defaultTheme = {
+  text: '#363537',
+  muted: '#4c4c4c',
+  toggleBorder: '#FFF',
+  background: '#FFF',
+  foreground: '#dcdcdc',
+  device: device
+};
